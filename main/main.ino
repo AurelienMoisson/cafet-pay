65e5f1e9fb5d7019c07b92cb7d68574d69876325
AurelienMoisson <44036815+AurelienMoisson@users.noreply.github.com>
2019-04-10 11:43:53 +0200

    added a serial terminal

--- main/main.ino
+++ main/main.ino
@@ -19,8 +19,6 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Approximate your card to the reader...");
-  Serial.println();
 
 }
 
@@ -35,15 +33,7 @@ String lookup_card(){
   {
     return "";
   }
-  //Show UID on serial monitor
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  return content;
+  return mfrc522.uid.uidByte;
 }
 
 void loop() 
