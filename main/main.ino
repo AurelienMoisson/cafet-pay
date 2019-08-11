307c88ba53cfbd9f0e3ba9a44e433a05052e4810
aurelien <44036815+AurelienMoisson@users.noreply.github.com>
2019-08-11 15:57:42 +0200

    removed lcd library

--- main/main.ino
+++ main/main.ino
@@ -11,8 +11,6 @@
 #include <MFRC522.h>
 
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 #define SS_PIN 10
@@ -24,11 +22,6 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-
-  //lcd screen
-  lcd.begin(0x27, 16, 2);
-  lcd.backlight();
-
 }
 
 String lookup_card(){
@@ -50,15 +43,7 @@ const String base = "0123456789ABCDEF";
 void loop() 
 {
   String content = lookup_card();
-  if (Serial.available()) {
-    lcd.clear();
-    char to_write = Serial.read();
-    while (to_write != 10) {
-      lcd.write(to_write);
-      to_write = Serial.read();
-    }
-  }
-  lcd.write("card!");
+
   for (int i=0; content[i] != 0; i++) {
     Serial.print(base[content[i]>>4]);
     Serial.print(base[content[i]&&15]);
