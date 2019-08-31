f114b50f7dab82328c6da4d0edc3cb31aac6a2f4
aurelien <44036815+AurelienMoisson@users.noreply.github.com>
2019-08-31 20:15:33 +0200

    made basic product selection interface

--- frontend/src/components/orderForm.js
+++ frontend/src/components/orderForm.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
+import products from "../products";
+import "../styles/form.scss"
 
 function Form() {
+  let [selected, setSelected] = useState({})
+  console.log(selected)
+
+  function addItem(product) {
+    var newSelected = {...selected}
+    if (selected[product.id]) {
+      newSelected[product.id] += 1;
+    } else {
+      newSelected[product.id] = 1;
+    }
+    setSelected(newSelected);
+    console.log('addItem called', selected)
+  }
+
   return (
-    <Button> test </Button>
+    <div className="form">
+      <div className="flex-container">
+        {getButtons(addItem)}
+      </div>
+      <div>
+        {showSelected(selected)}
+      </div>
+    </div>
+  )
+}
+
+function getButtons(addItem) {
+  let results = [];
+  for (var product of products) {
+    results.push(getIndividualButton(product, addItem))
+  }
+  return results;
+}
+
+function getIndividualButton(product, addItem) {
+  return (
+    <Button onClick={()=>{addItem(product)}} key={product.id}> {product.name} </Button>
+  )
+}
+
+function showSelected(selected) {
+  var result = []
+  console.log("showSelected:",selected)
+  for (var id in selected) {
+    console.log(id)
+    result.push(selectedProduct(id, selected[id]))
+  }
+  console.log("showSelected called")
+  return result
+}
+
+function selectedProduct(id, number) {
+  return (
+    <div>
+      <div>
+        {id}, {number}
+      </div>
+      <Button onClick={()=>{}}/>
+    </div>
   )
 }
 
---
+++ frontend/src/products.js
@@ -0,0 +1,7 @@
+const products = [
+        {id:1, name:"sandwich 1/3", price:1.5},
+        {id:2, name:"sandwich 1/2", price:2},
+        {id:3, name:"sandwich 2/3", price:2.5},
+]
+
+export default products;
