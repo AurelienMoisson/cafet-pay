0422b21dd2b8d27a6a44a2fd463f4c82aca4c92e
aurelien <44036815+AurelienMoisson@users.noreply.github.com>
2019-09-09 03:15:54 +0200

    capitalized products

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,14 +1,14 @@
 export function getProducts() {
   return [
-    {id:1, category:"sandwich", name:"1/3", price:1.5},
-    {id:2, category:"sandwich", name:"1/2", price:2},
-    {id:3, category:"sandwich", name:"2/3", price:2.5},
-    {id:4, category:"boisson", name:"oasis orange", price:0.6},
-    {id:5, category:"boisson", name:"oasis tropical", price:0.6},
-    {id:6, category:"boisson", name:"coca", price:0.6},
-    {id:7, category:"boisson", name:"coca zero", price:0.6},
-    {id:8, category:"boisson", name:"coca cherry", price:0.6},
-    {id:9, category:"boisson", name:"schweppes", price:0.6},
-    {id:10, category:"boisson", name:"orangina", price:0.6},
+    {id:1, category:"Sandwich", name:"1/3", price:1.5},
+    {id:2, category:"Sandwich", name:"1/2", price:2},
+    {id:3, category:"Sandwich", name:"2/3", price:2.5},
+    {id:4, category:"Boisson", name:"Oasis orange", price:0.6},
+    {id:5, category:"Boisson", name:"Oasis tropical", price:0.6},
+    {id:6, category:"Boisson", name:"Coca", price:0.6},
+    {id:7, category:"Boisson", name:"Coca zero", price:0.6},
+    {id:8, category:"Boisson", name:"Coca cherry", price:0.6},
+    {id:9, category:"Boisson", name:"Schweppes", price:0.6},
+    {id:10, category:"Boisson", name:"Orangina", price:0.6},
   ]
 }
