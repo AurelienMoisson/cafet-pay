91a2fc0a7d9fbb1cfa7ae6e3cc893ada3bc5da91
aurelien <44036815+AurelienMoisson@users.noreply.github.com>
2019-09-13 21:24:40 +0200

    added a cancel button

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -3,14 +3,16 @@ import Button from 'react-bootstrap/Button';
 
 import Total from './total';
 import Validate from './validate';
+import Cancel from './cancel';
 
 import '../styles/footer.scss';
 
-export default function Footer({selected, products, reductions}) {
+export default function Footer({selected, products, reductions, setSelected}) {
   return (
     <div className='footer'>
       <Total selected={selected} products={products} reductions={reductions}/>
-      <Validate selected={selected} products={products} reductions={reductions}/>
+      <Validate selected={selected} products={products} reductions={reductions} setSelected={setSelected}/>
+      <Cancel setSelected={setSelected}/>
     </div>
   )
 }
